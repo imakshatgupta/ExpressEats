bbd42834ee8043111dadf300eea57d32b55e8f5d
Akshat Gupta <akshatgtc@gmail.com>
2023-07-29 01:34:11 +0530

    initial

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export const Home = () => {
+  return (
+    <div>
+    <Navbar/>
+    Body
+    <Footer/>
+    </div>
+  )
+}
